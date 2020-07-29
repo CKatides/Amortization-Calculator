cd95f27ac6b996704a68cc7456d9541db9762cb3
Chris Katides <>
2020-07-28 22:59:38 -0400

    Initial commit

---
+++ Amortization.py
@@ -0,0 +1,55 @@
+import os
+import sys
+
+"""
+A is the payment amount per period, based on the included formula
+r is the interest rate per year
+P is the initial principal/loan amount
+n is the total number of payments or periods.
+
+A = P((r(1+r)**n)/((1+r)**n - 1))
+
+"""
+
+def showtable(P,r,n,A):
+    startingprincipal = P
+    monthlyinterest = r
+    count = 1
+    while P > 0:
+        interestpayment = monthlyinterest * P
+        principalpayment = A - interestpayment
+        remainingbalance = P - principalpayment
+        if remainingbalance <= 0:
+            break
+        else:
+            print("Payment " + str(count) + " Interest: $%.2f" % (interestpayment) + " Principal: $%.2f" % (principalpayment) + " Balance: $%.2f" % (remainingbalance))
+            P = remainingbalance
+            count += 1
+    
+
+print("\nWhat is the total loan amount?\n")
+P = input()
+
+print("\nWhat is the annual interest rate, expressed as a decimal?\n")
+r = input()
+
+print("\nWhat is the total amount of payments in months?\n")
+n = input()
+
+P = int(P)
+r = float(r)
+n = int(n)
+
+r = r/12 #convert to monthly interest
+A = (P*((r*((1+r)**n))/(((1+r)**n) - 1))) #Find payment amount
+
+print("\nThe payment amount per period is $%.2f" % (A))
+print("\n")
+tablechoice = input("Display data table...y/n? ")
+tablechoice = str(tablechoice)
+print("\n")
+
+if tablechoice in ('y','Y','yes','Yes'):
+    showtable(P,r,n,A)
+else:
+    print("No table! Ok!")
